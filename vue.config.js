441ba4e594307ee817d8e4eef75deb3ff3079a94
Dav Cousin <david-cousin@wanadoo.fr>
2020-11-14 17:06:15 +0100

    remove pwa

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,3 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  pwa: {
-    name: 'Todo List',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    assetsVersion: 'v0.4',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
-  }
+  transpileDependencies: ["vuetify"]
 };
